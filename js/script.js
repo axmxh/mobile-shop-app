7ea3d66a27d719e8798cfc9bd445b702a74499db
axmxh <axmxh@hotmail.com>
2016-05-27 03:52:48 +0200

    details added

--- js/script.js
+++ js/script.js
@@ -1,14 +1,14 @@
 var mobiles = [
-						{brand: "Sony" ,model: "Xperia L" , year:2013 ,memory:16 , color:"Gold" , sim:true ,nfc:true ,network:"4G" ,screan:5 },
-						{brand: "Samsung" ,model: "Note 3" , year:2014 ,memory:32 , color:"Black" , sim:true ,nfc:false ,network:"3G" ,screan:6},
-						{brand: "Apple" ,model: "6S" , year:2015 ,memory:16 , color:"White" , sim:true ,nfc:false ,network:"4G" ,screan:4},
-						{brand: "Nokia",model: "E71" , year:2009 ,memory:16 , color:"Black" , sim:true ,nfc:false ,network:"3G" ,screan:4},
-						{brand: "LG",model: "G4" , year:2015 ,memory:16 , color:"White" , sim:true ,nfc:true ,network:"4G" ,screan:6},
-						{brand: "Sony" ,model: "Xperia M" , year:2013 ,memory:16 , color:"Gold" , sim:true ,nfc:true ,network:"4G" ,screan:5 },
-						{brand: "Samsung" ,model: "Note 4" , year:2015 ,memory:32 , color:"Black" , sim:true ,nfc:false ,network:"3G" ,screan:6},
-						{brand: "Apple" ,model: "SE" , year:2016 ,memory:64 , color:"Gold" , sim:true ,nfc:false ,network:"4G" ,screan:4},
-						{brand: "Nokia",model: "Lumia" , year:2014 ,memory:32 , color:"White" , sim:true ,nfc:true ,network:"4G" ,screan:6},
-						{brand: "LG",model: "G5" , year:2016 ,memory:64 , color:"Black" , sim:true ,nfc:true ,network:"4G" ,screan:6}
+						{brand: "Sony" ,model: "Xperia L" , year:2013 ,memory:16 , color:"Gold" , sim:"Yes" ,nfc:"Yes" ,network:"4G" ,screen:5 },
+						{brand: "Samsung" ,model: "Note 3" , year:2014 ,memory:32 , color:"Black" , sim:"Yes" ,nfc:false ,network:"3G" ,screen:6},
+						{brand: "Apple" ,model: "6S" , year:2015 ,memory:16 , color:"White" , sim:"Yes" ,nfc:false ,network:"4G" ,screen:4},
+						{brand: "Nokia",model: "E71" , year:2009 ,memory:16 , color:"Black" , sim:"Yes" ,nfc:false ,network:"3G" ,screen:4},
+						{brand: "LG",model: "G4" , year:2015 ,memory:16 , color:"White" , sim:"Yes" ,nfc:"Yes" ,network:"4G" ,screen:6},
+						{brand: "Sony" ,model: "Xperia M" , year:2013 ,memory:16 , color:"Gold" , sim:"Yes" ,nfc:"Yes" ,network:"4G" ,screen:5 },
+						{brand: "Samsung" ,model: "Note 4" , year:2015 ,memory:32 , color:"Black" , sim:"Yes" ,nfc:false ,network:"3G" ,screen:6},
+						{brand: "Apple" ,model: "SE" , year:2016 ,memory:64 , color:"Gold" , sim:"Yes" ,nfc:false ,network:"4G" ,screen:4},
+						{brand: "Nokia",model: "Lumia" , year:2014 ,memory:32 , color:"White" , sim:"Yes" ,nfc:"Yes" ,network:"4G" ,screen:6},
+						{brand: "LG",model: "G5" , year:2016 ,memory:64 , color:"Black" , sim:"Yes" ,nfc:"Yes" ,network:"4G" ,screen:6}
 					];
 
 				
@@ -16,7 +16,7 @@ var mobileShop = angular
 				.module("shopModule", [])
 				.controller("mobiles",function($scope){					
 					$scope.mobiles = mobiles;
-					
+					$scope.showDetails = true;
 					// Search 					
 
 					$scope.search = function(){
@@ -27,6 +27,9 @@ var mobileShop = angular
 						//alert($scope.temp);	
 						//return $scope.modelSearch;
 						//$scope.clear();
+						
+
+						
 					};
 					
 					// Clear for Next search
@@ -35,12 +38,16 @@ var mobileShop = angular
 						$scope.tempBrand = "";
 					};
 					
+					
 					$scope.setSelected = function(){
 						$scope.setSelected = function() {
-					        $scope.selected = this.mobile;
-					        //alert(this.mobile.brand);
+					        $scope.selected = this.mobile;       
+					        //return $scope.selected
+					        //alert($scope.showDetails);
+					        $scope.showDetails = false;
 					    };
 					};
+					
 				});
 				
 				
